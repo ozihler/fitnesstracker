756bd7d6be3239ac2abcce87e2d4ad8a119814da
Oliver Zihler <ozihler@gmail.com>
2020-04-21 17:57:36 +0200

    ??

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import {FitnessTypeSelectionComponent} from "./fitness-type-selection.component"
 
 
 const routes: Routes = [
-  {path: '', redirectTo: 'fitness-type-selection', pathMatch: 'full'},
+  {path: '', component: FitnessTypeSelectionComponent},
   {path: 'fitness-type-selection', component: FitnessTypeSelectionComponent},
   {path: 'workout', loadChildren: () => import("./workout/workout.module").then(m => m.WorkoutModule)}
 ];
