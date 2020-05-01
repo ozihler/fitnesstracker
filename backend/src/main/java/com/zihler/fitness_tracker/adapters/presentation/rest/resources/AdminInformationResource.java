7ef53f33d37db9a756ee425810db0b349b6fdb5e
Oliver Zihler <ozihler@gmail.com>
2020-05-01 15:13:37 +0200

    this works

--- backend/src/main/java/com/zihler/fitness_tracker/adapters/presentation/rest/resources/AdminInformationResource.java
+++ backend/src/main/java/com/zihler/fitness_tracker/adapters/presentation/rest/resources/AdminInformationResource.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/admin/instantiationTime")
+@RestController
 public class AdminInformationResource {
 
     private final long instantiationTime;
@@ -14,7 +14,7 @@ public class AdminInformationResource {
         this.instantiationTime = System.currentTimeMillis();
     }
 
-    @GetMapping
+    @GetMapping("/api/admin/instantiationTime")
     public long instantiationTime() {
         return this.instantiationTime;
     }
